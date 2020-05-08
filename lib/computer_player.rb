3f3d60dd42e0fb6f39263fac19fd78ff3835d5c9
metacoder87 <gary.miller@gmx.com>
2020-05-08 06:18:43 -0400

    Adds in the initialize method solution for computer_player

--- lib/computer_player.rb
+++ lib/computer_player.rb
@@ -4,3 +4,18 @@
 
 
     # computer_player
+
+    # ComputerPlayer#initialize
+
+# An instance of ComputerPlayer should have the same interface as a HumanPlayer. 
+# This means that both classes should have the same method names, but may implement 
+# these methods using different logic. Having a consistent interface for all types 
+# of players will allow us to use computers and humans interchangeably in a game 
+# with no annoying side-effects. The #initialize for a computer should be identical 
+# to that of a human.
+
+        attr_reader :mark_value 
+
+    def initialize(mark_value)
+        @mark_value = mark_value
+    end
