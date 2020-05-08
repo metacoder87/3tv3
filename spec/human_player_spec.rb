03ace7f994a8c0b9d7993e3afc8e8e1605cb226d
metacoder87 <gary.miller@gmx.com>
2020-05-08 14:38:48 -0400

    Updates signature

--- spec/human_player_spec.rb
+++ spec/human_player_spec.rb
@@ -1,6 +1,7 @@
             # meta_coder (Gary Miller) =)
             # gmiller052611@gmail.com
-            # https://github.com/metacoder87/App-Academy-Projects
+            # https://github.com/metacoder87/3tv3
+
 
 
 require "human_player"
@@ -44,4 +45,4 @@ end
 
             # meta_coder (Gary Miller) =)
             # gmiller052611@gmail.com
-            # https://github.com/metacoder87/App-Academy-Projects
+            # https://github.com/metacoder87/3tv3
