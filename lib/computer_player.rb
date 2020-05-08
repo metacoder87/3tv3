b3b607714a5a73b6d138668791ee33ae734a6f49
metacoder87 <gary.miller@gmx.com>
2020-05-08 06:28:15 -0400

    Adds in the get_position method solution for the computer_player.

--- lib/computer_player.rb
+++ lib/computer_player.rb
@@ -5,7 +5,7 @@
 
     # computer_player
 
-    # ComputerPlayer#initialize
+# ComputerPlayer#initialize
 
 # An instance of ComputerPlayer should have the same interface as a HumanPlayer. 
 # This means that both classes should have the same method names, but may implement 
@@ -18,4 +18,16 @@
 
     def initialize(mark_value)
         @mark_value = mark_value
+    end
+
+# ComputerPlayer#get_position(legal_positions)
+
+# Assume that this method takes in an array of positions as an argument. The method 
+# should return a random position from the array. To make gameplay clear, print a 
+# message saying the computer's mark along with the position they chose.
+
+    def get_position(legal_positions)
+        chosen = legal_positions.sample
+        puts "#{@mark_value} chose #{chosen}."
+        chosen
     end
